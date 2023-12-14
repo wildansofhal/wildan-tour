c733ad072994dd0d0aa9d2311dcc9ff8e1645c08
wildansofhal <wildansofhal@gmail.com>
2023-12-14 15:24:44 +0700

    update main blade

--- resources/views/lay-user/main.blade.php
+++ resources/views/lay-user/main.blade.php
@@ -83,9 +83,9 @@
                 <img src="{{ asset('assets/img/icons/logo2.svg') }}" class="logo" alt="logo {{ mine()['longTitle'] }}">
               </a>
               <p class="fs--1 text-secondary my-2 fw-medium">{{ mine()['longTitle'] }} ! What better way to explore the beauty and hidden secrets of this island than to tour with a local ! As well as the cheapest and best package prices in Bali !</p>
-              <a target="_blank" href="{{ mine()['linkWa']() }}">
+              <a target="_blank" href="https://api.whatsapp.com/send/?phone=6282115040168&text=Halo%20Admin%0Ahttp://127.0.0.1:8000&type=phone_number&app_absent=0">
                 <svg class="svg-inline--fa fa-whatsapp fa-w-14 fs-3 me-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="whatsapp" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg>
-                {{ mine()['number'] }}
+                +62 82115040168
               </a>
             </div>
             <div class="col-md-3 col-sm-6 mb-5">
@@ -94,12 +94,6 @@
                 <li class="mb-2"><a class="link-900 fs-1 fw-medium text-decoration-none" href="/">Home</a></li>
                 <li class="mb-2"><a class="link-900 fs-1 fw-medium text-decoration-none" href="{{ route('tour') }}">Tour</a></li>
               </ul>
-            </div>
-            <div class="col-md-3 col-sm-6 mb-5 order-lg-1 order-md-2">
-              <h2 class="fw-bold font-sans-serif display-2">Tripadvisor</h2>
-              <a href="https://www.tripadvisor.com/Attraction_Review-g297701-d19229102-Reviews-Dulbali_Tour_Travel-Ubud_Gianyar_Regency_Bali.html" target="blank">
-                <img src="{{ asset('assets/img/service/tripadvisor.jpg') }}" class="img-fluid rounded-1 mt-2" alt="tripadvisor {{ mine()['longTitle'] }}">
-              </a>
             </div>
           </div>
         </div><!-- end of .container-->  
